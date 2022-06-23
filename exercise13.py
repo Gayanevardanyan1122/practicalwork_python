4f53ba89ac32d6c4ca7fc0448ee95a65854fa7da
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-23 21:59:59 +0400

    Create exercise13.py

---
+++ exercise13.py
@@ -0,0 +1,5 @@
+pi = 3.14
+r = float(input("insert the radius of your cylinder: "))
+h = float(input("insert the high of your cylinder: "))
+cylinder_volume = pi * r**2 * h
+print(f'volume of your cylinder is equal to: {cylinder_volume:f2}')
