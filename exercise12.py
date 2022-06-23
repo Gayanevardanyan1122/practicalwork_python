77e12c3fda4b29df97cc1b9931ef4a68880b60ba
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-23 21:56:27 +0400

    Update exercise12.py

--- exercise12.py
+++ exercise12.py
@@ -1,7 +1,5 @@
-"""1 foot = 12 duym
-   1 foot = 1 / 3 yard
-   1 foot = 1 / 5280 mile """
-distance = float(input("insert  the distance with foots: "))
-print(f'the distance with duyms is equal to: {12 * distance} duym')
-print(f'the distance with  yards is equal to: {distance / 3} yard')
-print(f'the distance with miles is equal to: {distance / 5280} mile')
+high = float(input("insert your high with funts: "))
+print(f'your high with santimetres is: {12 * 2.54 * high} santimetre')
+
+high_duym = float(input("insert your high with duyms:"))
+print(f'your high with santimetres is: {2.54 * high_duym} santimetre')
