f24faf8d723c1e436c87663138711d5e34eeb7a3
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-23 21:47:53 +0400

    Create exercise10.py

---
+++ exercise10.py
@@ -0,0 +1,5 @@
+MPG = float(input("insert your car MPG: "))
+print(f'your fuel with L/100km is equal to: {337.5 * MPG} L/100km')
+""" 1 gallon = 5.4 litre\
+    1 mile = 1.60 km\
+    1 MPG = 337.5 L/100km """
