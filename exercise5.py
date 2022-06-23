b619774b24d95b32cb889a78e63b1e29593394c6
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-23 21:04:39 +0400

    Create exercise5.py

---
+++ exercise5.py
@@ -0,0 +1,8 @@
+def percent(bill:float, n:int):
+      """this function returns the n percent os bill"""
+      return bill * n / 100
+  
+bill = float(input("insert the amount of your bill: "))
+print(f'the tip is equal to: {percent(bill, 18):f2} $')
+print(f'the tax is equal to: {percent(bill, 20):f2} $')
+print(f'the bill is equal to: {bill:f2} $')
