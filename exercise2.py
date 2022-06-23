23297f3bcca348979e439ae60cd7e65e1733e74a
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-23 20:58:01 +0400

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+ area(lenght: float, wight:float):
+    return lenght * wight
+    
+x = float(input("Insert lenght: "))
+y= float(input("insert wight: "))
+
+print(f'the area of floor is {area(x, y):.f2} meter')
