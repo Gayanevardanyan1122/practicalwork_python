34e752b69f2ae094d5137af673ee2112536e69f6
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-26 00:07:27 +0400

    Update and rename same to same_parity.py

--- same_parity.py
+++ same_parity.py
@@ -1,10 +1,10 @@
 def same_parity(n:int, *args:int):
         '''this function returns the list that consists of the arguments of
-        this function that are divided to the first argument of this funtion.'''
+        this function that are divided to n.'''
         lst = []
         for num in args:
              if num % n == 0:
                  lst.append(num)
         return lst 
 
-print(same_parity(12, 48, 24, 12, 90))
+print(same_parity(11, 20, 44, 48))
